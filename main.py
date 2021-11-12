802a8cd4bf6ed6ee88692120ec370139e14f0a3d
Acácio Vasconcelos Telechi <acaciotelechi@gmail.com>
2021-11-12 00:28:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ class tjpr_scraper():
         itens = tab.find_elements_by_tag_name("tr")[1:]
         return itens
 
-    def get_dados_item(self, item):
+    def _get_dict_item(self, item):
         tab_dados = item.find_element_by_class_name("juris-tabela-dados")
         link = tab_dados.find_element_by_tag_name("a").get_attribute("href")
         dados = self._get_dados_item(tab_dados)
@@ -41,11 +41,27 @@ class tjpr_scraper():
     
     def _get_dados_item(self, dados_item):
         dados_split = dados_item.text.split("\n")
-        processo, tipo = self._tratar_tipo(dados_split[0])
-        relator = self._tratar_relator(dados_split[2])
-        cargo = dados_split[3]
-        orgao = self._tratar_orgao(dados_split[6])
-        data = self._tratar_data(dados_split[7])
+        try:
+            processo, tipo = self._tratar_tipo(dados_split[0])
+        except:
+            processo = ''
+            tipo = ''
+        try:
+            relator = self._tratar_relator(dados_split[2])
+        except:
+            relator = ''
+        try:
+            cargo = dados_split[3]
+        except:
+            cargo = ''
+        try:
+            orgao = self._tratar_orgao(dados_split[6])
+        except:
+            orgao = ''
+        try:
+            data = self._tratar_data(dados_split[7])
+        except:
+            data = ''
         return {
             "processo": processo,
             "tipo": tipo,
@@ -76,22 +92,30 @@ class tjpr_scraper():
     def _tratar_data(self, data):
         return data.split(' ')[2]
 
-    def get_dados(self):
-        itens = scraper._get_itens_tabela_resultado()
+    def get_dados(self, max_paginas = 10):
+        total_processos = self.get_total_processos()
         lista_ok = []
         lista_pendentes = []
-        for item in itens:
-            dados_item = scraper.get_dados_item(item)
-            if dados_item['ementa'] == 'Conteúdo pendente de análise e liberação para consulta pública.':
-                lista_pendentes.append(dados_item)
-            else:
-                lista_ok.append(dados_item)
+        for i in range(max_paginas):
+            itens = self._get_itens_tabela_resultado()
+            for item in itens:
+                dados_item = self._get_dict_item(item)
+                if dados_item['ementa'] == 'Conteúdo pendente de análise e liberação para consulta pública.':
+                    lista_pendentes.append(dados_item)
+                else:
+                    lista_ok.append(dados_item)
+            self.driver.find_element_by_class_name("arrowNextOn").click()
+        self.driver.quit()
+        print(f"Total de processos baixados: {len(lista_ok)} ok e {len(lista_pendentes)} sem ementa. \nTotal de procesos: {total_processos}")
         return lista_ok, lista_pendentes
+    
+    def get_total_processos(self):
+        return int(self.driver.find_element_by_class_name("navLeft").text.split(' ')[0])
 
 if __name__ == "__main__":
     url = 'https://portal.tjpr.jus.br/jurisprudencia/publico/pesquisa.do?actionType=pesquisar'
     scraper = tjpr_scraper()
     scraper.driver.get(url)
     lista_ok, lista_pendentes = scraper.get_dados()
-    with open('teste3.json', 'w', encoding='utf8') as f:
-        json.dump(lista_ok, f, ensure_ascii=False)
+    # with open('teste3.json', 'w', encoding='utf8') as f:
+    #     json.dump(lista_ok, f, ensure_ascii=False)
