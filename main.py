c48f2207dafcea305c5a1645a40ed9b137b6fb42
Acácio Vasconcelos Telechi <acaciotelechi@gmail.com>
2021-11-11 23:53:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
+import json
 
 class tjpr_scraper():
 
     def __init__(self, hide=True):
         self.driver = self._inicializar_drivier(hide)
+        self.base = None
     
     def _inicializar_drivier(self, hide):
         options = webdriver.ChromeOptions()
@@ -12,18 +14,23 @@ class tjpr_scraper():
         driver = webdriver.Chrome(options=options)
         return driver
 
-    def _get_itens_tabela_resultado(self, url):
-        self.driver.get(url)
-        tab = scraper.driver.find_elements_by_class_name("resultTable")[1]
+    def _get_itens_tabela_resultado(self):
+        tab = self.driver.find_elements_by_class_name("resultTable")[1]
         itens = tab.find_elements_by_tag_name("tr")[1:]
-        return itens, tab
+        return itens
 
-    def _get_dados_item(self, item):
-        dados_item = item.find_element_by_class_name("juris-tabela-dados")
-        link = dados_item.find_element_by_tag_name("a").get_attribute("href")
-        return dados_item.text, link
+    def get_dados_item(self, item):
+        tab_dados = item.find_element_by_class_name("juris-tabela-dados")
+        link = tab_dados.find_element_by_tag_name("a").get_attribute("href")
+        dados = self._get_dados_item(tab_dados)
+        ementa = self._get_ementa_item(item)
+        return {
+            "link": link,
+            "dados": dados,
+            "ementa": ementa
+        }
 
-    def _get_ementa_itam(self, item):
+    def _get_ementa_item(self, item):
         ementa_item = item.find_element_by_class_name("juris-tabela-ementa")
         try:
             ementa_item.find_element_by_tag_name("a").click()
@@ -32,55 +39,59 @@ class tjpr_scraper():
             ementa_item = ementa_item.text
         return ementa_item
     
-    def get_url_list(self, url):
-        self.driver.get(url)
-        dados = self.driver.find_elements_by_class_name("juris-tabela-propriedades")
-        return [dado.find_element_by_tag_name("a").get_attribute("href") for dado in dados]
-
-    def get_data_from_processo(self, url):
-        self.driver.get(url)
-        dados = self.driver.find_element_by_class_name('juris-dados-completos')
-        dados_list = dados.text.split('\n')
-        ementa = dados_list[-1]
-        for i, dado in enumerate(dados_list):
-            if "Processo" in dado:
-                split = dado.split(' ')
-                processo = split[1].strip()
-                tipo = split[2].replace('(', '').replace(')', '').strip()
-            if "Segredo" in dado:
-                segredo = dado.split(':')[1].strip()
-            if "Relator" in dado:
-                relator = dado.split(':')[1].strip()
-                relator_cargo = dados_list[i+1].strip()
-            if "Órgão Julgador" in dado:
-                orgao_julgador = dado.split(':')[1].strip()
-            if "Comarca" in dado:
-                comarca = dado.split(':')[1].strip()
-            if "Data do Julgamento" in dado:
-                data_julgamento = dado.split(': ')[1].strip()
-            if "Data da Publicação" in dado:
-                data_publicacao = dado.split(': ')[1].strip()
+    def _get_dados_item(self, dados_item):
+        dados_split = dados_item.text.split("\n")
+        processo, tipo = self._tratar_tipo(dados_split[0])
+        relator = self._tratar_relator(dados_split[2])
+        cargo = dados_split[3]
+        orgao = self._tratar_orgao(dados_split[6])
+        data = self._tratar_data(dados_split[7])
         return {
-            'processo': processo,
-            'tipo': tipo,
-            'segredo': segredo,
-            'relator': relator,
-            'relator_cargo': relator_cargo,
-            'orgao_julgador': orgao_julgador,
-            'comarca': comarca,
-            'data_julgamento': data_julgamento,
-            'data_publicacao': data_publicacao,
-            'ementa': ementa
+            "processo": processo,
+            "tipo": tipo,
+            "relator": relator,
+            "cargo": cargo,
+            "orgao": orgao,
+            "data": data
         }
+    
+    def _tratar_tipo(self, processo):
+        split = processo.split(' ')
+        numero = split[1]
+        tipo = split[3].replace('(', '') + ' ' + split[4].replace(')', '')
+        return numero, tipo
+    
+    def _tratar_relator(self, relator):
+        split = relator.split(' ')
+        r = split[2:]
+        r = ' '.join(r)
+        return r
 
+    def _tratar_orgao(self, orgao):
+        split = orgao.split(' ')
+        o = split[2:]
+        o = ' '.join(o)
+        return o
+
+    def _tratar_data(self, data):
+        return data.split(' ')[2]
+
+    def get_dados(self):
+        itens = scraper._get_itens_tabela_resultado()
+        lista_ok = []
+        lista_pendentes = []
+        for item in itens:
+            dados_item = scraper.get_dados_item(item)
+            if dados_item['ementa'] == 'Conteúdo pendente de análise e liberação para consulta pública.':
+                lista_pendentes.append(dados_item)
+            else:
+                lista_ok.append(dados_item)
+        return lista_ok, lista_pendentes
 
 if __name__ == "__main__":
     url = 'https://portal.tjpr.jus.br/jurisprudencia/publico/pesquisa.do?actionType=pesquisar'
     scraper = tjpr_scraper()
-    url_list = scraper.get_url_list(url)
-    print(url_list)
-    lista_final = []
-    for url in url_list:
-        dados = scraper.get_data_from_processo(url)
-        lista_final.append(dados)
-    print(lista_final)
+    scraper.driver.get(url)
+    lista_ok, lista_pendentes = scraper.get_dados()
+    with open('teste3.json', 'w', encoding='utf8') as f:
+        json.dump(lista_ok, f, ensure_ascii=False)
